feacec9f0735c8a43e61edb85e692daeeb2c1a5c
ItsJonaCR <jonavasquez.cr@gmail.com>
2025-10-22 10:44:50 -0600

    New Project. Add configuration files and initial structure of the Spring Boot project

---
+++ src/main/java/com/jonathan/springboot/calendar/interceptor/springboot_schedule/SpringbootScheduleApplication.java
@@ -0,0 +1,13 @@
+package com.jonathan.springboot.calendar.interceptor.springboot_schedule;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootScheduleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootScheduleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jonathan/springboot/calendar/interceptor/springboot_schedule/SpringbootScheduleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jonathan.springboot.calendar.interceptor.springboot_schedule;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootScheduleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
